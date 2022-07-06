74dab76b2d7bff142035b0dfbb6a3f63e523d8d6
EWassmann <107703321+EWassmann@users.noreply.github.com>
2022-07-06 17:02:55 -0400

    Add files via upload

--- makeafencesimple.py
+++ makeafencesimple.py
@@ -1,15 +1,18 @@
-# from re import X
+# importing all of the needed packages
 from shapely.geometry import Polygon, Point
 import multiprocessing as mp
 import serial
 from ublox_gps import UbloxGps
 import threading
 import time
+#setting up the port and baudrate for the gps (serial communications), baudrate is what sparkfun reccomended on github
 port1 = serial.Serial('/dev/ttyACM1', baudrate=38400, timeout=1)
 gps = UbloxGps(port1)
+#initalizing the counter for the Geofence coordinats list as well as the Geofence coordinates list
 counter = 0
 Geofencecoordinates = list()
 
+#setting up the serial communications with the arduino
 arduino = serial.Serial(
 port = '/dev/ttyACM0',
 baudrate = 2000000, #perhaps make this lower need to do research
@@ -22,7 +25,8 @@ rtscts = False,
 dsrdtr = False,
 writeTimeout = 2
 )
-
+#these are all the functions that write to the arduino that tell it to move, the numbers are sent as a 
+#string and the arduino will decode them on that side
 #----------------Functions---------------------------
 def Left():
     arduino.write("1".encode()) 
@@ -48,6 +52,8 @@ def Search():
     arduino.write("5".encode())
     global b
     b = 5
+#this is the inital thread it is used to store the gps coordinates of a track the robot takes, and assighn them to a list containing tuples that 
+#shapely can use
 def track(stop):
     print("Beginning to track")
     while True:
@@ -65,11 +71,14 @@ def track(stop):
             counter = counter + 1
         except (ValueError, IOError) as err:
             print(err)
+            #this breaks out of the function
         if stop():
             break
+#starting the thread for creating the fence
 stop_threads = False
 t1 = threading.Thread(target = track, args = (lambda: stop_threads, ))
 t1.start()
+#this is the second thread it just keeps track of the robots positon so part two of the code can see if it is in or out of the fence
 
 def track2(stop2):
     print("Beginning to track")
@@ -89,7 +98,7 @@ def track2(stop2):
             break
 stop_threads2 = False
 t2 = threading.Thread(target = track2, args = (lambda: stop_threads2, )) 
-
+#loop for making the fence
 while True:
     
         
@@ -99,6 +108,7 @@ while True:
 
 
     directions = input()
+    #stuff below is from keyboard controll it is looking for the inputs to run themovement functions from the top
 
 
     if directions == "w":
@@ -111,6 +121,8 @@ while True:
         Stop()
     if directions == "ss":
         Back() 
+    #q is gonna set the last point to the first point (shapely wants this) end the first thread and determine the center of the fence
+    #center of the fence isnt used here but may be used in later iterations of this code
     if directions== "q":
         stop_threads = True
         Geofencecoordinates.insert(counter, Geofencecoordinates[0])
@@ -119,9 +131,10 @@ while True:
         cent = (GeoFence.centroid)
         print(cent)
         break
-
+#starting the second thread
 t2.start()
 while True:
+    #this allows  you to drive the robot around using wasd untill you exit the fence
     directions = input()
     RobotCoord = Point(a,b)
     if GeoFence.contains(RobotCoord) == True:
@@ -138,7 +151,8 @@ while True:
     else:
         Back()
         time.sleep(4)
-    
+        Stop()
+    #this ends the threads and closes out the port
     if directions == "q":
             stop_threads2 = True
             port1.close()
